419048a6d8e1b6541f44780672a240c60eebc820
Kiko Beats <josefrancisco.verdu@gmail.com>
2017-12-24 09:43:22 +0000

    Resolve all production deps

--- src/index.js
+++ src/index.js
@@ -1,20 +1,18 @@
 'use strict'
 
-const path = require('path')
-
+const getProductionDeps = require('./getProductionDeps')
 const loadConfig = require('./load-config')
 
 const doNotIgnore = dep => `!${dep}`
 
 module.exports = async ({ cwd }) => {
-  const { dependencies } = require(path.resolve(cwd, 'package.json'))
   const { blacklist, whitelist } = await loadConfig({ cwd })
 
   const removeBlacklistedDeps = dep => !blacklist.includes(dep)
   const includeNamespaces = dep =>
     dep.indexOf('/') >= 0 ? dep.split('/')[0] : dep
 
-  const productionDeps = Object.keys(dependencies)
+  const productionDeps = getProductionDeps()
     .filter(removeBlacklistedDeps)
     .map(includeNamespaces)
 
